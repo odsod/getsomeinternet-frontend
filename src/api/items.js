6bfe6ce84880e927bf9f3bbc2857fa4c234b4aac
Oscar Söderlund <poscar@spotify.com>
2015-01-02 23:23:04 +0100

    Hook back up to backend

--- src/api/items.js
+++ src/api/items.js
@@ -1,26 +1,27 @@
 var Q = require('q');
+var request = require('superagent');
 
-//request('/api/items', function(err, res) {
-  //if (err || res.xhr.status < 200 || res.xhr.status >= 300) {
-    //console.log('epic fail.. adding test items');
-    //items = items.concat(testItems);
-    //return;
-  //}
-  //console.log('mmm.. got items', res.body);
-  //items = items.concat(res.body);
-//});
+var testItems = [
+  {type: 'gif', data: 'http://media.giphy.com/media/1Anamuv8XEimQ/giphy.gif', score: 182},
+  {title: 'I\'ve been thinking about you (Dilemmachine Remix)', type: 'soundcloud', data: '94160734', score: 7},
+  {title: 'Everyone is not Jesus', type: 'youtube', data: 'GllR1WyDzJc', score: 81},
+  {type: 'gif', data: 'https://s3.amazonaws.com/giphymedia/media/d8qrbB7GKlhza/giphy.gif', score: 12},
+  {title: 'Arnold\'s tank', type: 'youtube', data: 'jVs5kgvA_Ow', score: 9}
+  //{type: 'gif', data: 'https://s3.amazonaws.com/giphymedia/media/QUDH4Aepdq4U0/giphy.gif', score: 13},
+  //{type: 'gif', data: 'https://s3.amazonaws.com/giphymedia/media/hzN96x2i8R2GQ/giphy.gif', score: 14},
+  //{type: 'gif', data: 'https://s3.amazonaws.com/giphymedia/media/GzjFAsz1DjGog/giphy.gif', score: 15}
+];
 
 exports.loadMoreItems = function() {
   var deferredItems = Q.defer();
-  deferredItems.resolve([
-    {type: 'gif', data: 'http://media.giphy.com/media/1Anamuv8XEimQ/giphy.gif', score: 182},
-    {title: 'I\'ve been thinking about you (Dilemmachine Remix)', type: 'soundcloud', data: '94160734', score: 7},
-    {title: 'Everyone is not Jesus', type: 'youtube', data: 'GllR1WyDzJc', score: 81},
-    {type: 'gif', data: 'https://s3.amazonaws.com/giphymedia/media/d8qrbB7GKlhza/giphy.gif', score: 12},
-    {title: 'Arnold\'s tank', type: 'youtube', data: 'jVs5kgvA_Ow', score: 9}
-    //{type: 'gif', data: 'https://s3.amazonaws.com/giphymedia/media/QUDH4Aepdq4U0/giphy.gif', score: 13},
-    //{type: 'gif', data: 'https://s3.amazonaws.com/giphymedia/media/hzN96x2i8R2GQ/giphy.gif', score: 14},
-    //{type: 'gif', data: 'https://s3.amazonaws.com/giphymedia/media/GzjFAsz1DjGog/giphy.gif', score: 15}
-  ])
+  request('/api/items', function(err, res) {
+    if (err || res.xhr.status < 200 || res.xhr.status >= 300) {
+      console.log('failed to get items.. using test items instead');
+      deferredItems.resolve(testItems);
+    } else {
+      console.log('got items from backend', res.body);
+      deferredItems.resolve(res.body);
+    }
+  });
   return deferredItems.promise;
 };
